e4d7e714f9095e56b1a672d393e3858cdade9d83
leonevil <leongb66@gmail.com>
2020-08-03 13:04:10 -0300

    User entity and resource

--- src/main/java/com/example/course/CourseApplication.java
+++ src/main/java/com/example/course/CourseApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CourseApplication {
 
+	// https://stackoverflow.com/questions/22380119/why-does-my-spring-boot-app-always-shutdown-immediately-after-starting
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CourseApplication.class, args);
 	}
